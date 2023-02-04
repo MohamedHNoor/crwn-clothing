1c2fc16daea5e48ccb652dc4ff21ca5fe0728d4c
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2023-02-04 09:55:22 +0200

    Add logo, navigation and sign-in components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => {
+  return <h1>Shop page</h1>;
+};
 
 const App = () => {
-  
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="Shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
-  
 };
 
 export default App;
